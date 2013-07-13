5dbea183577707700c6933322f29a82177ee1b30
Ubuntu <ubuntu@ip-172-31-6-172.us-west-2.compute.internal>
2013-07-13 16:36:31 +0000

    fixed web.js, redeploy small index.html

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 var page_content = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello Super World!');
+  // response.send('Hello Super World!');
+  response.send(page_content);
 });
 
 var port = process.env.PORT || 5000;
